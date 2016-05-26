7f02422145b9851333351e084d5ec847e1920cd9
Іван Шварьов <desdi4ado@hotmail.com>
2016-05-26 20:05:35 +0300

    Стандартный класс List

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,11 +10,26 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            Point p1 = new Point(11, 3, '*');           
+            Point p2 = new Point(12, 5, '#');
             
-            Point p2 = new Point(2, 5, '#');
-            p2.Draw();
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            foreach (int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
 
             Console.ReadLine();
         }
