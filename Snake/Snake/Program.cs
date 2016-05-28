03a2a22451c04621cfefe1bf8c07a589f5be6bea
Іван Шварьов <desdi4ado@hotmail.com>
2016-05-28 12:14:42 +0300

    Создание класса HorizontalLine

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -12,24 +12,12 @@ namespace Snake
         {
             Point p1 = new Point(11, 3, '*');           
             Point p2 = new Point(12, 5, '#');
-            
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
 
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
+            p1.Draw();
+            p2.Draw();
 
-            foreach (int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
+            HorizontalLine line = new HorizontalLine(1, 20, 7, '+');
+            line.Drow();
 
             Console.ReadLine();
         }
