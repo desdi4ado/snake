5601b63ea6e2f7e333325990636ef282ebf3fbb3
Іван Шварьов <desdi4ado@hotmail.com>
2016-05-31 21:17:44 +0300

    Создание класса FoodCreator Движение Змейки и увеличение после поедания))

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -26,23 +26,35 @@ namespace Snake
 
             // отрисовка точек
             Point p = new Point(4, 5, '*');
-
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Drow();
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
 
-            Console.ReadLine();
+            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
+
+            while(true)
+            {
+                if(snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
+
+                Thread.Sleep(100);
+
+                if(Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
+                }
+            }
+
+            //Console.ReadLine();
         }
     }
 }
