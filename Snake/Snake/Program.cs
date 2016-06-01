6cea42cbc6f783b7ed7c3df16d9e625046d58ca1
Іван Шварьов <desdi4ado@hotmail.com>
2016-06-01 10:27:35 +0300

    Понятие полиморфизма

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -11,7 +11,7 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            // установка размера окна
+            /*// установка размера окна
             Console.SetBufferSize(80, 25);
 
             // отрисовка рамки
@@ -55,6 +55,34 @@ namespace Snake
             }
 
             //Console.ReadLine();
+            */
+            VerticalLine v1 = new VerticalLine(0, 10, 5, '%');
+            Draw(v1);
+
+            Point p = new Point(4, 5, '*');
+            Figure fSnake = new Snake(p, 4, Direction.RIGHT);
+            Draw(fSnake);
+            Snake snake = (Snake)fSnake;
+
+            HorizontalLine h1 = new HorizontalLine(0, 5, 6, '&');
+
+            List<Figure> figures = new List<Figure>();
+            figures.Add(fSnake);
+            figures.Add(v1);
+            figures.Add(h1);
+
+            foreach(var f in figures)
+            {
+                f.Draw();
+            }
+
+            Console.ReadLine();
         }
+
+        static void Draw(Figure figure)
+        {
+            figure.Draw();
+        }
+
     }
 }
