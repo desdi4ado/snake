8e0ae6095108921ad12bfd7926c1a5de99fe26a2
Іван Шварьов <desdi4ado@hotmail.com>
2016-06-03 20:37:06 +0300

    Класс Hit (препятствия)

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -11,31 +11,29 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            /*// установка размера окна
+            // установка размера окна
             Console.SetBufferSize(80, 25);
 
-            // отрисовка рамки
-            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
-            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
-            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
-            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
-            upLine.Drow();
-            downLine.Drow();
-            leftLine.Drow();
-            rightLine.Drow();
+            Walls walls = new Walls(80, 25);
+            walls.Draw();
 
             // отрисовка точек
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
-            snake.Drow();
+            snake.Draw();
 
             FoodCreator foodCreator = new FoodCreator(80, 25, '$');
             Point food = foodCreator.CreateFood();
             food.Draw();
 
-            while(true)
+            while (true)
             {
-                if(snake.Eat(food))
+                if(walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
+
+                if (snake.Eat(food))
                 {
                     food = foodCreator.CreateFood();
                     food.Draw();
@@ -47,42 +45,13 @@ namespace Snake
 
                 Thread.Sleep(100);
 
-                if(Console.KeyAvailable)
+                if (Console.KeyAvailable)
                 {
                     ConsoleKeyInfo key = Console.ReadKey();
                     snake.HandleKey(key.Key);
                 }
             }
-
+        }
             //Console.ReadLine();
-            */
-            VerticalLine v1 = new VerticalLine(0, 10, 5, '%');
-            Draw(v1);
-
-            Point p = new Point(4, 5, '*');
-            Figure fSnake = new Snake(p, 4, Direction.RIGHT);
-            Draw(fSnake);
-            Snake snake = (Snake)fSnake;
-
-            HorizontalLine h1 = new HorizontalLine(0, 5, 6, '&');
-
-            List<Figure> figures = new List<Figure>();
-            figures.Add(fSnake);
-            figures.Add(v1);
-            figures.Add(h1);
-
-            foreach(var f in figures)
-            {
-                f.Draw();
-            }
-
-            Console.ReadLine();
-        }
-
-        static void Draw(Figure figure)
-        {
-            figure.Draw();
-        }
-
     }
 }
